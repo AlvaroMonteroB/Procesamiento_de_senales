7c00faad2db96b330353002ef0aec157c9adbf27
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-09-06 12:04:36 -0600

    Init

--- P1/Sinusoidal_signal.m
+++ P1/Sinusoidal_signal.m
@@ -1,8 +1,8 @@
-function sinusoidal=Sinusoidal_signal(freq,amp,fase,dur,mps)
+function sinusoidal=Sinusoidal_signal(freq,amp,fase,dur,mps,name)
     t=linspace(0,dur,dur*mps);
     signal=amp*sin(2*pi*freq*t+fase);
     plot(t, signal);
-    title('Señal Senoidal');
+    title(name);
     xlabel('Tiempo (s)');
     ylabel('Amplitud');
     grid on;
