fc65218bd50f71bd479d021c55a572ec804f46b5
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-09-06 12:26:48 -0600

    todos los incisos

--- P1/Sinusoidal_signal.m
+++ P1/Sinusoidal_signal.m
@@ -1,9 +1,7 @@
 function sinusoidal=Sinusoidal_signal(freq,amp,fase,dur,mps,name)
     t=linspace(0,dur,dur*mps);
     signal=amp*sin(2*pi*freq*t+fase);
-    plot(t, signal);
-    title(name);
-    xlabel('Tiempo (s)');
-    ylabel('Amplitud');
-    grid on;
+    func_plot(t,signal,name,'Tiempo (s)','Amplitud')
+   sinusoidal=signal;
+end
     
---
+++ P1/func_plot.m
@@ -0,0 +1,7 @@
+function plotting=func_plot(t,signal,title_str,xlabel_str,ylabel_str)
+    plot(t, signal);
+    title(title_str);
+    xlabel(xlabel_str);
+    ylabel(ylabel_str);
+    grid on;
+    
